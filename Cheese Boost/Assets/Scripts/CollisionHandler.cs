66970613914dc40ade93e48e1d832312428ae1de
04wakeup <dojun849@gmail.com>
2021-05-10 19:57:46 -0700

    reload goto next level

--- Cheese Boost/Assets/Scripts/CollisionHandler.cs
+++ Cheese Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,5 +1,6 @@
 ﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {  
@@ -10,11 +11,31 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Start");
                 break;
             case "Fuel":
-                Debug.Log("Fuel");
+                Debug.Log("Fuel"); 
+                break;
+            case "Finish":
+                LoadNextLevel();
                 break;
             default:
-                Debug.Log("what did you touch???");
+                ReloadLevel(); 
                 break;
         }
     }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneInex = currentSceneIndex + 1;
+        if (nextSceneInex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneInex = 0;
+        } 
+        SceneManager.LoadScene(nextSceneInex);
+    }
+
 }
