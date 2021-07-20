72a3d3e4e0511223703392745295e22713fde75d
04wakeup <dojun849@gmail.com>
2021-07-19 19:40:03 -0700

    NaN handling

--- Cheese Boost/Assets/Scripts/Oscillator.cs
+++ Cheese Boost/Assets/Scripts/Oscillator.cs
@@ -20,14 +20,17 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period;  // continually growing over time
-        const float tau = Mathf.PI * 2; // constant value of 6.283
-        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+        if (period <= Mathf.Epsilon) {  return; }
+            float cycles = Time.time / period;  // continually growing over time
+            const float tau = Mathf.PI * 2; // constant value of 6.283
+            float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
 
-        movmentFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
+            movmentFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
 
-        Vector3 offset = movementVector * movmentFactor;
-      
-        transform.position = startingPosition + offset * goBack; 
+            Vector3 offset = movementVector * movmentFactor;
+        
+            transform.position = startingPosition + offset * goBack; 
+    
+       
     }
 }
